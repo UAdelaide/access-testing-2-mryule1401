82b023ccc2644f236fe619b143db3e6121917457
Mryule1401 <lehuy011406@gmail.com>
2025-06-18 11:04:27 +0000

    [automatic commit] 2025-06-18 11:04:26 UTC 129.127.202.117

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/posts', function(req, res, next) {
 
+router.get('/posts', function(req, res, next) {
   if('user' in req.session){
       console.log(req.session.user);
   }
