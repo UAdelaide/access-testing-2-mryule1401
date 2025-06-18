f4ff033f3e2341a2afacdaf1772c014b24170562
Mryule1401 <lehuy011406@gmail.com>
2025-06-18 05:23:23 +0000

    [automatic commit] 2025-06-18 05:23:23 UTC 129.127.202.117

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -19,7 +19,7 @@ router.post('/messages', async function(req, res) {
     await db.query(`
         INSERT INTO Messages (BuyerID, SellerID, BookID, MessageText,SentAt)
         VALUES (?, ?, ?, ?, NOW())
-    `, [CURRENT_BUYER_ID, CURRENT_SELLER_ID, bookID, message]);
+    `, [CURRENT_BUYER_ID, sellerID, bookID, message]);
     res.status(201).json({ message: 'Message sent successfully' });
 });
 module.exports = router;
