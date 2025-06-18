0420922911aceb5462fe398572b62a6e4d2d1a2b
Mryule1401 <lehuy011406@gmail.com>
2025-06-18 04:12:56 +0000

    [automatic commit] 2025-06-18 04:12:56 UTC 129.127.202.117

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -6,12 +6,13 @@ var CURRENT_BUYER_ID =1;
 var CURRENT_SELLER_ID =2;
 
 router.get('/items', async function(req, res) {
-    const [rows] =await db.query(`
-        SELECT bl.BookID,  bl.Title, b.Author, u.Name AS SellerName, b.Price
-        FROM BookListings bl
-        JOIN BookInfo b ON bl.BookInfoID = b.BookInfoID
-        JOIN Users u ON bl.SellerID = u.UserID`);
-        res.json(rows);
+const [rows] = await db.query(`
+    SELECT bl.BookID, b.Title, b.Author, u.Name AS SellerName, bl.Price
+    FROM BookListings bl
+    JOIN BookInfo b ON bl.BookInfoID = b.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+`);
+res.json(rows);
 });
 
 module.exports = router;
