0e2dc55878d9140f8b8e0bbc6a6cfa63607bddc8
Mryule1401 <lehuy011406@gmail.com>
2025-06-18 03:38:58 +0000

    [automatic commit] 2025-06-18 03:38:57 UTC 129.127.202.117

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -7,4 +7,5 @@ var CURRENT_SELLER_ID =2;
 
 router.get('/items', async function(req, res) {
     const [rows] =await db.query(`
-        SELECT b.BookID, b.Title, b.Author, u`)
+        SELECT b.BookID, b.Title, b.Author, u.Name AS SellerName, b.Price
+        F`)
