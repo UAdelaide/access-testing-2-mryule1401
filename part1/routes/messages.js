05da823c258fbee87866ce16108c428debbea4c8
Mryule1401 <lehuy011406@gmail.com>
2025-06-18 03:37:14 +0000

    [automatic commit] 2025-06-18 03:37:14 UTC 129.127.202.117

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -5,4 +5,6 @@ var db = require('../db');
 var CURRENT_BUYER_ID =1;
 var CURRENT_SELLER_ID =2;
 
-router.get('/items', async function(req, res, next) {
+router.get('/items', async function(req, res) {
+    const [rows] =await db.query;
+    
